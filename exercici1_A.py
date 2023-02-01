8b1f1a1b3a43b3d6cb06fcfa2e28335ce6f937c9
Talía <tlopez22@jaumebalmes.net>
2023-02-01 20:29:26 +0100

    Práctica 10

--- exercici1_A.py
+++ exercici1_A.py
@@ -1,2 +1,3 @@
+#Funció que donat l'any de naixement et calcula l'edat actual
 def any(anyNaixement):
     return 2023 - anyNaixement
--- exercici1_B.py
+++ exercici1_B.py
@@ -1,3 +1,4 @@
+#Creem la funció any, necessitem la variable anysPersona
 def any(anysPersona):
+    #Fem return de 2023 - anysPersona que li hem donat valor desde el main en la funció
     return 2023 - anysPersona
-    print(any(anysPersona))
